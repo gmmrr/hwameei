56d2c6b7044210da33180fae00afd3eafdb9eff5
Gmmrr <guanming091429@gmail.com>
2023-04-29 19:52:43 +0800

    0429 1952 update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var first_time = 1;
+var first_time_hamburger_icon = 1;
 function show_hamburger_content() {
     var hamburger_content = document.getElementById("hamburger_content");
     var hamburger_content_shadow = document.getElementById("hamburger_content_shadow");
@@ -10,9 +10,45 @@ function show_hamburger_content() {
         hamburger_content.style.display = "none";
         hamburger_content_shadow.style.display = "none";
     }
-    if (first_time) {
+    if (first_time_hamburger_icon) {
         hamburger_content.style.display = "block";
         hamburger_content_shadow.style.display = "block";
-        first_time = !first_time;
+        first_time_hamburger_icon = !first_time_hamburger_icon;
     }
 }
+
+
+var first_time_homepage_switching = 1;
+let homepage_switching = anime({
+    targets: '#homepage_background',
+    scale: '1.02',
+    easing: 'easeOutQuart',
+    duration: '1100',
+    endDelay: '1900'
+});
+var homepage_index = 1;
+function homepage_change(){
+    
+    pic = document.getElementById("homepage_background");
+    pic.style.backgroundImage = `url("src/homepage_backgnd${homepage_index}.jpg")`;
+    if(!first_time_homepage_switching) {
+        homepage_switching.restart();
+    }
+
+    if(homepage_index >= 3){
+        homepage_index = 1;
+    } else {
+        homepage_index++;
+    }
+
+    first_time_homepage_switching = 0;
+}
+setInterval(homepage_change,3000);
+
+
+
+// anime({
+//     targets: '#homepage_background',
+//     easing: 'easeOutQuad'
+// });
+
