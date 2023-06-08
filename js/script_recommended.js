2502384395b4657bf105db5456071be340b2b90e
Gmmrr <guanming091429@gmail.com>
2023-06-08 17:20:48 +0800

    new update

--- js/script_recommended.js
+++ js/script_recommended.js
@@ -61,6 +61,8 @@ function get_tag() {
                 let data = xhr.response;
                 button_chosen_array = data;
                 console.log(button_chosen_array);
+                show_chosen_tag();
+                choose_three_items();
             } else {
                 console.log("request failed");
             }
@@ -74,7 +76,38 @@ function get_tag() {
     });
 }
 get_tag();
-
+function show_chosen_tag() {
+    let tag_name = [
+        "流線型",
+        "速度感",
+        "時尚",
+        "簡約",
+        "復古",
+        "前衛",
+        "奢華",
+        "貓眼",
+        "黑",
+        "白",
+        "金",
+        "銀",
+        "米",
+        "藍",
+        "粉",
+        "跑步",
+        "騎行",
+        "鐵人三項",
+        "室內",
+        "閱讀",
+        "休閒",
+    ];
+    for (let i = 0; i < 21; i++) {
+        if (button_chosen_array[i] == true) {
+            console.log(tag_name[i]);
+            document.getElementById("button_chosen").innerHTML +=
+                '<div class= "button_chosen_element">' + tag_name[i] + "</div>";
+        }
+    }
+}
 function choose_three_items() {
     let item_chosen = new Array(3);
     for (let i = 0; i < 3; i++) {
@@ -94,10 +127,11 @@ function choose_three_items() {
         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
         [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
         [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
         [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
     ];
 
     for (let i = 0; i < 12; i++) {
@@ -108,28 +142,26 @@ function choose_three_items() {
         }
     }
 
-    console.log(item_similarity);
-
     let item_chosen_index = [];
     let sortedIndices = Array.from(item_similarity.keys()).sort((a, b) => item_similarity[b] - item_similarity[a]);
     let topThreeIndices = sortedIndices.slice(0, 3);
 
     for (let i = 0; i < topThreeIndices.length; i++) {
-        item_chosen_index.push(item_similarity[topThreeIndices[i]]);
+        item_chosen_index.push(topThreeIndices[i]);
     }
 
     console.log(item_chosen_index);
 }
-choose_three_items();
 
-let glasses_artemis = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0];
-let glasses_caelum = [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0];
-let glasses_dogma_ocean = [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1];
-let glasses_echo_black = [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
-let glasses_kamikaze = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0];
-let glasses_ode_ocean = [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
-let glasses_vseries_black = [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0];
-let glasses_vseries_pink = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0];
-let glasses_vseries_planet_silver = [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1];
-let glasses_vseries_white = [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0];
-let glasses_wi6970 = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0];
+// let glasses_artemis = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0];
+// let glasses_caelum = [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0];
+// let glasses_dogma_ocean = [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1];
+// let glasses_echo_black = [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
+// let glasses_kamikaze = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0];
+// let glasses_ode_ocean = [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
+// let glasses_vseries_black = [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0];
+// let glasses_vseries_planet_silver = [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1];
+// let glasses_vseries_white = [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0];
+// let glasses_wi6970 = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0];
+// let glasses_kilmer = [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
+// let glasses_magnus = [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1];
